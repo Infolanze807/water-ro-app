020782a83cd996c7b1d002e521e9959236a5c70c
trushal1601 <trushal.prajapati1601@gmail.com>
2024-08-05 12:52:55 +0530

    solved

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
 import { View, Text } from "react-native";
 import React, { useEffect, useState } from "react";
 import Sign_up from "./src/Pages/Regestration/Sign_up";
-import SplashScreen from "./src/Components/SplashScreen/SplashScreen";
-
 import { createStackNavigator } from "@react-navigation/stack";
 import Home from "./src/Pages/Home/Home";
 import Sign_in from "./src/Pages/Login/Sign_in";
@@ -12,15 +10,13 @@ const App = () => {
   const Stack = createStackNavigator();
 
   return (
-    <View className="pt-7">
       <NavigationContainer>
-        <Stack.Navigator initialRouteName="sign-up">
+        <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown:false}}>
           <Stack.Screen name="Home" component={Home} />
           <Stack.Screen name="sign-up" component={Sign_up} />
           <Stack.Screen name="sign-in" component={Sign_in} />
         </Stack.Navigator>
       </NavigationContainer>
-    </View>
   );
 };
 
