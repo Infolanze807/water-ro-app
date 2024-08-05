a8b74a8d2716854293ac51b9d466c540da90a2bf
nishant-timbadiya <nishanttimbadiya987@gmail.com>
2024-08-05 13:07:48 +0530

    Navigation

--- src/Pages/Regestration/Sign_up.js
+++ src/Pages/Regestration/Sign_up.js
@@ -1,58 +1,86 @@
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, ImageBackground } from 'react-native'
-import React from 'react'
-import Entypo from '@expo/vector-icons/Entypo';
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import Entypo1 from '@expo/vector-icons/Entypo';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  StyleSheet,
+  ImageBackground,
+  ScrollView,
+} from "react-native";
+import React from "react";
+import Entypo from "@expo/vector-icons/Entypo";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import Entypo1 from "@expo/vector-icons/Entypo";
 import img from "../../../assets/images/Sign_up_bg.png";
 
 const Sign_up = () => {
   return (
-    <View>
-          <ImageBackground source={img} resizeMode='cover'>
-        <View className='p-6 pt-11 bg-primar h-72'>
-      <Text className="text-[18px] text-white">Hello,</Text>
-      <Text className='text-[35px] text-white'>Sign Up!</Text>
-      </View>
-      <View className='p-10  flex gap-4'>
-        <View>
-        <Text>Mobile Number</Text>
-        <TextInput placeholder='Enter a mobile number' className='border border-gray-400 rounded-lg p-1 px-2'/>
+    <ScrollView>
+      <ImageBackground source={img} resizeMode="cover">
+        <View className="p-6 pt-11 bg-primar h-72">
+          <Text className="text-[18px] text-white">Hello,</Text>
+          <Text className="text-[35px] text-white">Sign Up!</Text>
         </View>
-        <View>
-        <Text>City</Text>
-        <TextInput placeholder='Enter a city' keyboardType='numeric' className='border border-gray-400 rounded-lg p-1 px-2'/>
+        <View className="p-10  flex gap-4">
+        <View className="">
+            <Text className="mb-1 text-base text-gray-700">Mobile Number</Text>
+            <View className="flex flex-row items-center border border-gray-300 rounded-lg p-1.5">
+              <Entypo name="phone" size={18} color="gray" className="mr-2" />
+              <Text className='pl-2 text-base'>+91</Text>
+              <TextInput 
+                placeholder="Enter Your Phone" 
+                className="flex-1 text-base pl-1"
+                keyboardType="phone-pad"
+              />
+            </View>
+          </View>
+          <View className="">
+            <Text className="mb-1 text-base text-gray-700">City</Text>
+            <View className="flex flex-row items-center border border-gray-300 rounded-lg p-1.5">
+              <Entypo name="location-pin" size={20} color="gray" className="mr-2" />
+              <TextInput 
+                placeholder="Enter City Name" 
+                className="flex-1 text-base pl-1.5"
+              />
+            </View>
+          </View>
+          <View className="">
+            <Text className="mb-1 text-base text-gray-700">Company Name</Text>
+            <View className="flex flex-row items-center border border-gray-300 rounded-lg p-1.5">
+              <FontAwesome name="building" size={18} color="gray" className="mr-2" />
+              <TextInput 
+                placeholder="Enter Company Name" 
+                className="flex-1 text-base pl-2"
+              />
+            </View>
+          </View>
         </View>
-        <View>
-        <Text>Company Name</Text>
-        <TextInput placeholder='Enter a company name' className='border border-gray-400 rounded-lg p-1 px-2'/>
+        <View className="flex justify-center items-center bg-white">
+          <TouchableOpacity style={styles.button}>
+            <Text className="text-white text-center text-[18px]">Sign up</Text>
+          </TouchableOpacity>
         </View>
-      </View>
-      <View className="flex justify-center items-center bg-white">
-        <TouchableOpacity style={styles.button}>
-          <Text className="text-white text-center text-[18px]">Sign up</Text>
-        </TouchableOpacity>
-      </View>
-      <View className='flex flex-row gap-6 justify-center mt-5'>
-      <Entypo name="twitter-with-circle" size={28} color='blue'/>
-      <FontAwesome name="google-plus-official" size={28} color="red" />
-      <Entypo1 name="linkedin-with-circle" size={28} color="blue" />
-      </View>
-          </ImageBackground>
-    </View>
-  )
-}
+        <View className="flex flex-row gap-6 justify-center mt-5">
+          <Entypo name="twitter-with-circle" size={28} color="blue" />
+          <FontAwesome name="google-plus-official" size={28} color="red" />
+          <Entypo1 name="linkedin-with-circle" size={28} color="blue" />
+        </View>
+      </ImageBackground>
+    </ScrollView>
+  );
+};
 
 const styles = StyleSheet.create({
-    button: {
-      backgroundColor: '#3034E9', // Adjust this color to match your primary color
-      padding: 12,
-      borderRadius: 9999,
-      width: 220,
-      shadowColor: '#000',
-      shadowOffset: { width: 0, height: 2 },
-      shadowOpacity: 0.8,
-      shadowRadius: 3.84,
-      elevation: 5,
-    },
-  });
-export default Sign_up
+  button: {
+    backgroundColor: "#3034E9", 
+    padding: 12,
+    borderRadius: 9999,
+    width: 220,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+});
+export default Sign_up;
